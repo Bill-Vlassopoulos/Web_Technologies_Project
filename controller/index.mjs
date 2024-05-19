72ea4320a57bbf90e2adad3b844664412fd751ec
ggeorgiouu <up1089878@ac.upatras.gr>
2024-05-19 17:55:34 +0300

    style add Exhibition

--- controller/index.mjs
+++ controller/index.mjs
@@ -9,7 +9,7 @@ import bodyParser from 'body-parser'
 const model = await import('../model/queries.mjs')
 
 const app = express()
-const port = process.env.PORT || '3001';
+const port = process.env.PORT || '3000';
 const router = express.Router();
 
 const __filename = fileURLToPath(import.meta.url);
